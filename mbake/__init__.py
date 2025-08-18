d6e140287c25c726f8611ebc239861a06a88a748
Ebod Shojaei <ebod.shojaei@alumni.ubc.ca>
2025-08-18 03:35:43 -0700

    (fix) validate relative includes (#52)

--- mbake/__init__.py
+++ mbake/__init__.py
@@ -1,6 +1,6 @@
 """mbake - A Python-based Makefile formatter and linter."""
 
-__version__ = "1.4.0"
+__version__ = "1.4.1.pre"
 __author__ = "mbake Contributors"
 __description__ = "A Python-based Makefile formatter and linter"
 
