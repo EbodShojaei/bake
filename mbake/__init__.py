3649ff19d547424e7cae668216423cb90d802715
EbodShojaei <ebod.shojaei@alumni.ubc.ca>
2025-10-19 18:44:05 -0700

    (update) pre-release v1.4.3rc0

--- mbake/__init__.py
+++ mbake/__init__.py
@@ -1,6 +1,6 @@
 """mbake - A Python-based Makefile formatter and linter."""
 
-__version__ = "1.4.2.post1"
+__version__ = "1.4.3rc0"
 __author__ = "mbake Contributors"
 __description__ = "A Python-based Makefile formatter and linter"
 
